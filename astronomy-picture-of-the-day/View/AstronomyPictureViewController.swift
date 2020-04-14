000c27dcb4d1339e3bb0c5a34e28856e6cb284c7
Pamela <pamela.ianovalli@ifood.com.br>
2020-04-14 13:03:30 -0300

    Refactoring in layout

--- astronomy-picture-of-the-day/View/AstronomyPictureViewController.swift
+++ astronomy-picture-of-the-day/View/AstronomyPictureViewController.swift
@@ -31,26 +31,30 @@ class AstronomyPictureViewController: UIViewController {
     }
     
     func setupLayout(){
-        titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant:  100).isActive = true
-        titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true
-        titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant:  -40).isActive = true
         titleLabel.textColor = UIColor.purple
         titleLabel.font = .systemFont(ofSize: 20, weight: .bold)
         titleLabel.textAlignment = .center
-        imageView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 30).isActive = true
-        imageView.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor).isActive = true
-        imageView.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor).isActive = true
-        imageView.heightAnchor.constraint(equalToConstant: 300).isActive = true
-        imageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        label.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 30).isActive = true
-        label.leadingAnchor.constraint(equalTo: imageView.leadingAnchor).isActive = true
-        label.trailingAnchor.constraint(equalTo: imageView.trailingAnchor).isActive = true
+        
         label.setContentHuggingPriority(UILayoutPriority.required, for: .horizontal)
         label.contentCompressionResistancePriority(for: .horizontal)
         label.numberOfLines = 0
         label.lineBreakMode = .byWordWrapping
-        label.heightAnchor.constraint(greaterThanOrEqualToConstant: 100).isActive = true
-
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant:  100),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            
+            imageView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 30),
+            imageView.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor),
+            imageView.heightAnchor.constraint(equalToConstant: 300),
+            imageView.widthAnchor.constraint(equalToConstant: 200),
+            label.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 30),
+            label.leadingAnchor.constraint(equalTo: imageView.leadingAnchor),
+            label.trailingAnchor.constraint(equalTo: imageView.trailingAnchor),
+            label.heightAnchor.constraint(greaterThanOrEqualToConstant: 100)
+        ])
     }
     
     override func viewDidAppear(_ animated: Bool) {
